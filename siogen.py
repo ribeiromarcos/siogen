51146289b9f64ac336b225bd316d5ef1cb7ebf0b
marcos <marcos@ultrapcifmg>
2017-09-14 16:52:33 -0300

    Initial code

---
+++ siogen.py
@@ -0,0 +1,164 @@
+#!/usr/bin/python -u
+# -*- coding: utf-8 -*-
+'''
+SIOgen: Simple Insert Delete Dataset Generator
+'''
+
+import random
+import csv
+
+# Parameters
+ATT = 'att'
+INS = 'ins'
+DEL = 'del'
+FILE = 'file'
+
+# Default parameter values
+ATT_DEFAULT = 10
+INS_DEFAULT = 10000
+DEL_DEFAULT = 1000
+FILE_DEFAULT = 'output.csv'
+
+
+def gen_insertions(par_dict):
+    '''
+    Generate insertions
+    '''
+    # Current key
+    current_key = 0
+    # List of records
+    rec_list = []
+    # Loop to create insertions
+    for _ in range(par_dict[INS]):
+        # New record
+        new_record = {}
+        # List of attributes
+        att_list = ['A' + str(number + 1) for number in range(par_dict[ATT])]
+        # Generate each attribute value
+        for att in att_list:
+            new_record[att] = random.randint(0, par_dict[INS])
+        # Overwrite the attribute key
+        new_record['A1'] = current_key
+        # Increase key value
+        current_key += 1
+        # Insertion flag
+        new_record['OP'] = '+'
+        # Append record to list
+        rec_list.append(new_record)
+    return rec_list
+
+
+def gen_insertions_deletions(insertion_list, par_dict):
+    '''
+    Generate final record list with insertions and deletions
+    '''
+    # Shuffle record list
+    random.shuffle(insertion_list)
+    # List of insertions and deletions
+    rec_list = []
+    # List of current records (excluding deleted record)
+    current_list = []
+    # Group of insertions before a deletion
+    group_len = par_dict[INS] / par_dict[DEL]
+    # Process insertion list
+    while len(insertion_list):
+        # Copy a group of insertions to current list
+        current_list += insertion_list[:group_len]
+        # Copy a group of insertions to final list
+        rec_list += insertion_list[:group_len]
+        # Shufle current list
+        random.shuffle(current_list)
+        # Remove first record from current list
+        rec = current_list.pop()
+        # Create a copy of removed recod
+        del_rec = rec.copy()
+        # mark the record to be deleted
+        del_rec['OP'] = '-'
+        # Append deleted record into final list
+        rec_list.append(del_rec)
+        # Remove group of processed record from insertion list
+        del insertion_list[:group_len]
+    return rec_list
+
+
+def store_records(rec_list, par_dict):
+    '''
+    Store a record list in into file
+    '''
+    # Check if record list is empty
+    if not len(rec_list):
+        return
+    # Build attribute list including operation flag
+    att_list = ['OP'] + \
+        ['A' + str(number + 1) for number in range(par_dict[ATT])]
+    # Open file
+    out_file = open(par_dict[FILE], 'w')
+    # Create csv writer
+    out_write = csv.DictWriter(out_file, att_list)
+    # Write file header
+    out_write.writeheader()
+    # Write records
+    out_write.writerows(rec_list)
+    # Close file
+    out_file.close()
+
+
+def gen_data(par_dict):
+    '''
+    Generate data
+    '''
+    # Generate insertions
+    insertion_list = gen_insertions(par_dict)
+    # Generate final record list (with insertions and deletions)
+    rec_list = gen_insertions_deletions(insertion_list, par_dict)
+    # Store initial list on file
+    store_records(rec_list, par_dict)
+
+
+def get_arguments(print_help=False):
+    '''
+    Get arguments
+    '''
+    import argparse
+    parser = argparse.ArgumentParser('SIOGen')
+    parser.add_argument('-a', '--attributes', action='store', type=int,
+                        default=ATT_DEFAULT,
+                        help='Number of attributes (default: ' +
+                        str(ATT_DEFAULT) + ')')
+    parser.add_argument('-i', '--insertions', action='store', type=int,
+                        default=INS_DEFAULT,
+                        help='Number of insertions (default: ' +
+                        str(INS_DEFAULT) + ')')
+    parser.add_argument('-d', '--deletions', action='store', type=int,
+                        default=DEL_DEFAULT,
+                        help='Number of deletions (default: ' +
+                        str(DEL_DEFAULT) + ')')
+    parser.add_argument('-f', '--filename', action='store', type=str,
+                        default=FILE_DEFAULT,
+                        help='Output filename (default: ' +
+                        FILE_DEFAULT + ')')
+    args = parser.parse_args()
+    if print_help:
+        parser.print_help()
+    return args
+
+
+def main():
+    '''
+    Main routine
+    '''
+    # Get arguments
+    args = get_arguments()
+    # Create dictionary of parameters
+    par_dict = {
+        ATT: args.attributes,
+        INS: args.insertions,
+        DEL: args.deletions,
+        FILE: args.filename
+        }
+    # Generate data
+    gen_data(par_dict)
+
+
+if __name__ == '__main__':
+    main()
